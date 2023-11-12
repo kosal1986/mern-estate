071a8d4c588fd1695698ac7596fa6c4c3f046e07
Kosal t <kosalpoesr@gmail.com>
2023-11-12 17:18:02 +0700

    install

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sigin from "./pages/Sigin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <h1 className="text-red-600 text-3xl font-bold underline">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Sigin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
